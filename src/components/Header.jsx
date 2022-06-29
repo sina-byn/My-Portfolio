916061a00c2a558b0ce35dfd0910802ae5d92bfb
sina-byn <sina.sina.bayan@protonmail.com>
2022-06-29 15:46:02 +0430

    main: Header padding-inline for small screens Fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import Navbar from './Navbar';
 
 const Header = () => {
     return (
-        <header className='flex justify-between h-[10%] px-16 py-5'>
+        <header className='flex justify-between h-[10%] px-16 py-5 730:px-8'>
             <Logo />
             <Navbar />
         </header>
