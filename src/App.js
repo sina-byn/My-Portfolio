9d6de81f09fff455b2adfff5264fc99fb0f8d87f
sina-byn <sina.sina.bayan@protonmail.com>
2022-06-30 14:20:24 +0430

    main: NotFound Page Added

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import { Route, Routes, Navigate } from "react-router-dom";
 // Importing Pages
 import HomePage from "./pages/HomePage";
 import AboutMe from "./pages/AboutMe";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <div className='container w-full max-w-[1400px] min-h-screen text-light mx-auto'>
       <Routes>
-        <Route path="/about-me" element={<AboutMe />} />
+        <Route path='/about-me' element={<AboutMe />} />
         <Route path='/' element={<HomePage />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
   );
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+
+// Importing Styles
+import styles from "./NotFound.module.scss";
+
+const NotFound = () => {
+  const navigate = useNavigate();
+  const errorCodeRef = useRef(null);
+
+  useEffect(() => {
+    const visual = errorCodeRef.current;
+    const events = ["resize", "load"];
+
+    events.forEach(function (e) {
+      window.addEventListener(e, function () {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        const ratio = 45 / (width / height);
+        visual.style.transform =
+          "translate(-50%, -50%) rotate(-" + ratio + "deg)";
+      });
+    });
+  }, []);
+
+  return (
+    <main className={styles.main}>
+      <button
+        onClick={() => navigate(-1)}
+        type='button'
+        className={styles.back_button}
+      >
+        <svg
+          height='0.8em'
+          width='0.8em'
+          viewBox='0 0 2 1'
+          preserveAspectRatio='none'
+        >
+          <polyline
+            fill='none'
+            stroke='#777777'
+            strokeWidth='0.1'
+            points='0.9,0.1 0.1,0.5 0.9,0.9'
+          />
+        </svg>
+        Home
+      </button>
+      <div className={styles.background_wrapper}>
+        <h1 ref={errorCodeRef} className={styles.error_code}>
+          404
+        </h1>
+      </div>
+      <p className={styles.message}>
+        The page you’re looking for does not exist.
+      </p>
+    </main>
+  );
+};
+
+export default NotFound;
