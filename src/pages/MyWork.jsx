1cac799be0ce3801b4ea1f8ffd5179513f53b3e1
sina-byn <sina.sina.bayan@protonmail.com>
2022-07-30 13:17:04 +0430

    main: MyWork section padding bottom changed

--- src/pages/MyWork.jsx
+++ src/pages/MyWork.jsx
@@ -15,7 +15,7 @@ const MyWork = () => {
   return (
     <MainLayout>
       <main className='h-[85vh] 730px:pr-0 pr-[8px] pb-[20px]'>
-        <div className='content-wrap grid grid-cols-3 gap-[30px] w-full max-h-full overflow-y-auto pr-[52px] pl-[60px] pt-[10px] pb-[20px] 730px:px-[30px] 1030px:px-[20px] 1030px:grid-cols-2 1030px:gap-[20px] 600px:grid-cols-1 600px:gap-y-5 600px:gap-x-0'>
+        <div className='content-wrap grid grid-cols-3 gap-[30px] w-full max-h-full overflow-y-auto pr-[52px] pl-[60px] pt-[10px] pb-[20px] 730px:px-[30px] 1030px:px-[20px] 1030px:grid-cols-2 1030px:gap-[20px] 600px:grid-cols-1 600px:gap-y-5 600px:gap-x-0 pb-20 sm:pb-0'>
           {CARDS_DATA.map((card, idx) => {
             return (
               <Card
